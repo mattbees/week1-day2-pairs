e8b3112b9494e4369fc9538bd240d55096dbd650
Matthew <a9852147@9852147s-MacBook-Pro.local>
2019-04-03 23:42:23 +0100

    Include evening work

--- lab_functions.js
+++ lab_functions.js
@@ -37,7 +37,7 @@ function evenNums(numArray) {
   for ( var number of numArray) {
     if (number%2 === 0) {
       evens.push(number);
-      console.log("hi");
+//    console.log("hi");
     }
   }
   return evens;
@@ -84,7 +84,27 @@ console.log(total(myBill));
 
 // 2.b. Find meals by customer name - Create a function that takes in a bill and a customer name as an argument and returns a new array containing all the items for that customer.
 
+const findMeals = (billArray, name) => {
+  const customerMeals = [];
+  for (var bill of billArray) {
+    if (name === bill.customer) {
+      customerMeals.push(bill.meal);
+    }
+  }
+  return customerMeals;
+};
+
+console.log('Meals: ' + findMeals(myBill, "Sally"));
+
 // 2.c. Using the functions you wrote in part 4 and 5, find the total that Sally spent.
+
+
+
+const totalCost = (billArray, name) => {
+  const meals = findMeals(billArray, name);
+  // loop through billArray checking for each meal
+  // if found,
+};
 
 
 // 2.d. Find average item price - Create a function that takes in bill as an argument and calculates the average price of all the items.
@@ -97,12 +117,23 @@ console.log(total(myBill));
 var bodyBuilder = {
   strength: 2,
   workOut: (mins) => {
-   bodyBuilder.strength = bodyBuilder.strength * mins;
+    bodyBuilder.strength = bodyBuilder.strength * mins;
+  },
+  lazeAbout: (hours) => {
+    bodyBuilder.strength = bodyBuilder.strength / hours;
   }
 }
 
 // 3.a. Log out the the bodyBuilder's strength - the value should be 2.
 
+console.log(bodyBuilder.strength);
+
 // 3.b. Use the workOut function to update the bodyBuilder's strenth.
 
+bodyBuilder.workOut(5);
+console.log(bodyBuilder.strength);
+
 // 3.c. Create another function on the bodyBuilder called `lazeAbout`, that takes in a number of hours as an argument. Decrease the bodyBuilder's strength by dividing the strength by the number of hours.
+
+bodyBuilder.lazeAbout(2);
+console.log(bodyBuilder.strength);
